136c931bd8a4c47f7671f21846d8a5e760d0da36
Ahmed Aboauf <aaboauf@iskydev.com>
2022-07-07 12:54:02 +0200

    updates

--- env_prep_passwordless.sh
+++ env_prep_passwordless.sh
@@ -1,3 +1,6 @@
+useradd admin
+touch ./admin
+echo "admin ALL=(ALL) NOPASSWD:ALL" >> ./admin
 ansible all -m user -a "name=admin state=present" --user root --become --become-user root --become-method sudo
 ansible all -m copy -a "src=./admin dest=/etc/sudoers.d/" --user root --become --become-user root --become-method sudo
 ansible-playbook import-key.yml --user root --become --become-user root --become-method sudo
