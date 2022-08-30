c2b95337b35b26c478774133d02b84b9ee423ea5
Ahmed Aboauf <ahmed.aboauf@hotmail.com>
2022-08-30 23:01:36 +0200

    update

--- env_prep.sh
+++ env_prep.sh
@@ -1,3 +1,4 @@
-ansible all -m user -a "name=admin state=present" --user root --become --become-user root --become-method sudo
+ansible all -m user -a "name=admin state=present append=yes groups=wheel" --user root --become --become-user root --become-method sudo
 ansible all -m copy -a "src=./admin dest=/etc/sudoers.d/" --user root --become --become-user root --become-method sudo
-ansible-playbook import-key.yml --user root --become --become-user root --become-method sudo
+ansible all -m copy -a "content='ansible ALL=(ALL) NOPASSWD:ALL' dest=/etc/sudoers.d/ansible mode=400 owner=root" --user root --become --become-user root --become-method sudo
+# ansible-playbook import-key.yml --user root --become --become-user root --become-method sudo
